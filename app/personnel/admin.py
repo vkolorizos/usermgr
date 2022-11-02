498ab08cc5b71dc9d63be819e1e0e4ac887c38e6
carvanitis <carvanitis@TMOS-SIT-DEV1>
2022-11-02 19:32:00 +0200

    Add personnel options fields to admin panel

--- app/personnel/admin.py
+++ app/personnel/admin.py
@@ -2,18 +2,31 @@ from django.contrib import admin
 
 from personnel.models import Department, ConsentForm, PersonnelOptions
 
+
 # Register your models here.
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     fields = ('name',)
 
+
 class PersonnelOptionsAdminInline(admin.StackedInline):
     model = PersonnelOptions
     exclude = ()
 
+
 @admin.register(ConsentForm)
 class ConsentFormAdmin(admin.ModelAdmin):
     model = ConsentForm
-    exclude = ()
-    inlines = [PersonnelOptionsAdminInline,]
+    list_display = ('id', 'last_name', 'first_name', 'department', 'is_submitted', 'issue_toll_card', 'training')
+    inlines = [PersonnelOptionsAdminInline, ]
+
+    def issue_toll_card(self, x):
+        return bool(x.personnel_options.issue_toll_card)
+    issue_toll_card.short_description = 'Issue Toll Card'
+    issue_toll_card.boolean = True
+
+    def training(self, x):
+        return bool(x.personnel_options.training)
+    training.short_description = 'Training'
+    training.boolean = True
--- app/personnel/models.py
+++ app/personnel/models.py
@@ -8,25 +8,28 @@ class Department(models.Model):
         return self.name
 
 
+class ConsentForm(models.Model):
+    last_name = models.CharField(max_length=120)
+    first_name = models.CharField(max_length=120)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    is_submitted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.last_name + ' ' + self.first_name
+
+    class Meta:
+        ordering = ('-created_at',)
+
+
 class PersonnelOptions(models.Model):
     issue_toll_card = models.BooleanField(default=False)
     training = models.BooleanField(default=False)
     share_personal_email = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-
-
-class ConsentForm(models.Model):
-    last_name = models.CharField(max_length=120)
-    first_name = models.CharField(max_length=120)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    is_submitted = models.BooleanField(default=False)
-    personnel_options = models.OneToOneField(PersonnelOptions, on_delete=models.CASCADE,primary_key=True,)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    personnel_options = models.OneToOneField(ConsentForm, on_delete=models.CASCADE, primary_key=True, related_name='personnel_options')
 
     def __str__(self):
-        return self.last_name + '' + self.first_name
-
-    class Meta:
-        ordering = ('-created_at',)
+        return ''
